c232c18c50831d235fbb42e1d9a842c3bc30498b
nakagawa yuki <mstyk1021@gmail.com>
2026-01-07 00:42:03 +0900

    リモートリポジトリの変更

---
+++ src/resources/views/confirm.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css')}}" />>
+    <link rel="stylesheet" href="{{ asset('css/confirm.css')}}" />>
+</head>
+
+<body>
+    <header class="header">
+        <div class=="header__class"
+    </header>
+</body>
+</html>
