97a9d1411485c41b5d530e8a07b5191f8df344b9
nakagawa yuki <mstyk1021@gmail.com>
2026-01-09 02:25:22 +0900

    　テーブル作成

--- src/database/migrations/2026_01_09_020518_create_contacts_table.php
+++ src/database/migrations/2026_01_09_020518_create_contacts_table.php
@@ -14,10 +14,12 @@ class CreateContactsTable extends Migration
     public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
             $table->string('email');
-                $table->string('tel', 11);
+            $table->string('tel', 11);
             $table->text('content')->nullable();
+            $table->timestamps();
         });
     }
 
