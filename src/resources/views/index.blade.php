bb1460fd2a95f581062c1cf473b0fcfa28b2c81b
nakagawa yuki <mstyk1021@gmail.com>
2026-01-11 01:50:27 +0900

    　共通部分のレイアウト化

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,95 +1,78 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts')
 
-<head>
-<meta charset="UTF-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Contact Form</title>
-<link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+@section('css')
 <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
-</head>
+@endsection
 
-<body>
-<header class="header">
-    <div class="header__inner">
-    <a class="header__logo" href="/">
-        Contact Form
-    </a>
-    </div>
-</header>
+@section('Content')
 
-<main>
-    <div class="contact-form__content">
+<div class="contact-form__content">
     <div class="contact-form__heading">
         <h2>お問い合わせ</h2>
     </div>
     <form class="form" action="/contacts/confirm" method="post">
         @csrf
         <div class="form__group">
-        <div class="form__group-title">
-            <span class="form__label--item">お名前</span>
-            <span class="form__label--required">必須</span>
-        </div>
-        <div class="form__group-content">
-            <div class="form__input--text">
-            <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
+            <div class="form__group-title">
+                <span class="form__label--item">お名前</span>
+                <span class="form__label--required">必須</span>
             </div>
-            <div class="form__error">
-                @error('name')
-                {{ $message }}
-                @enderror
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
+                </div>
+                <div class="form__error">
+                    @error('name')
+                    {{ $message }}
+                    @enderror
+                </div>
             </div>
-        </div>
         </div>
         <div class="form__group">
-        <div class="form__group-title">
-            <span class="form__label--item">メールアドレス</span>
-            <span class="form__label--required">必須</span>
-        </div>
-        <div class="form__group-content">
-            <div class="form__input--text">
-            <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+                <span class="form__label--required">必須</span>
             </div>
-            <div class="form__error">
-                @error('email')
-                {{ $message }}
-                @enderror
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
+                </div>
+                <div class="form__error">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </div>
             </div>
-        </div>
         </div>
         <div class="form__group">
-        <div class="form__group-title">
-            <span class="form__label--item">電話番号</span>
-            <span class="form__label--required">必須</span>
-        </div>
-        <div class="form__group-content">
-            <div class="form__input--text">
-            <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}" />
+            <div class="form__group-title">
+                <span class="form__label--item">電話番号</span>
+                <span class="form__label--required">必須</span>
             </div>
-            <div class="form__error">
-                @error('tel')
-                {{ $message }}
-                @enderror
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}" />
+                </div>
+                <div class="form__error">
+                    @error('tel')
+                    {{ $message }}
+                    @enderror
+                </div>
             </div>
-        </div>
         </div>
         <div class="form__group">
-        <div class="form__group-title">
-            <span class="form__label--item">お問い合わせ内容</span>
-        </div>
-        <div class="form__group-content">
-            <div class="form__input--textarea">
-            <textarea name="content" placeholder="資料をいただきたいです"></textarea>
+            <div class="form__group-title">
+                <span class="form__label--item">お問い合わせ内容</span>
             </div>
-        </div>
+            <div class="form__group-content">
+                <div class="form__input--textarea">
+                <textarea name="content" placeholder="資料をいただきたいです"></textarea>
+                </div>
+            </div>
         </div>
         <div class="form__button">
-        <button class="form__button-submit" type="submit">送信</button>
+            <button class="form__button-submit" type="submit">送信</button>
         </div>
     </form>
     </div>
-</main>
-</body>
-
-</html>
+@endsection
