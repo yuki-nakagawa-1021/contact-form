5ed1ebaf82dcf91a17430a323adbe6aee9573e6d
nakagawa yuki <mstyk1021@gmail.com>
2026-01-11 03:23:11 +0900

    　共通部分のレイアウト化

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Form</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
     @yield('css')
 </head>
 
