ea47ee2c8c939aa16d312b2acb1d47538c4db890
nakagawa yuki <mstyk1021@gmail.com>
2026-02-06 04:50:32 +0900

    バリデーション設定

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -38,22 +38,22 @@ class ContactRequest extends FormRequest
     public function messages()
     {
         return [
-            'first_name.required' => '名前を入力してください',
+            'first_name.required' => '姓を入力してください',
             'first_name.string' => '名前を文字列で入力してください',
             'first_name.max' => '名前を8文字以下で入力してください',
-            'last_name.required' => '名前を入力してください',
+            'last_name.required' => '名を入力してください',
             'last_name.string' => '名前を文字列で入力してください',
             'last_name.max' => '名前を8文字以下で入力してください',
             'gender' => '性別を選択してください',
             'email.required' => 'メールアドレスを入力してください',
-            'email.email' => '有効なメールアドレス形式を入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
             'tel.required' => '電話番号を入力してください',
-            'tel.numeric' => '電話番号を数値で入力してください',
-            'tel.digits_between' => '電話番号を5桁以内で入力してください',
+            'tel.numeric' => '電話番号を半角数値で入力してください',
+            'tel.digits_between' => '電話番号は5桁まで数字で入力してください',
             'address.required' => '住所を入力してください',
             'category_id.required' => 'お問い合わせの種類を選択してください',
             'detail.required' => 'お問い合わせ内容を入力してください',
-            'detail.max' => 'お問い合わせの内容を120文字以下で入力してください',
+            'detail.max' => 'お問い合わせ内容を120文字以下で入力してください',
         ];
     }
 }
