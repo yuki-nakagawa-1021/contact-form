51b10387242a8ec4ccae79a904054ee0bbf7c853
nakagawa yuki <mstyk1021@gmail.com>
2026-02-06 04:17:41 +0900

    入力内容確認ページの送信ボタン

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -14,6 +15,15 @@ class ContactController extends Controller
     public function confirm(Request $request)
     {
         $contact = $request->only(['first_name', 'last_name', 'gender', 'email', 'tel', 'address', 'building', 'category_id', 'detail']);
+
         return view('confirm', compact('contact'));
+    }
+
+    public function store(Request $request)
+    {
+        $contact = $request->only(['first_name', 'last_name', 'gender', 'email', 'tel', 'address', 'building', 'category_id', 'detail']);
+        Contact::create($contact);
+
+        return view('thanks');
     }
 }
--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -9,15 +9,17 @@
     <div class="confirm__heading">
         <h2>Confirm</h2>
     </div>
-    <form class="form">
+    <form class="form" action="/thanks" method="POST">
+        @csrf
         <div class="confirm-table">
             <table class="confirm-table__inner">
                 <tr class="confirm-table__row">
                     <th class="confirm-table__header">お名前</th>
                     <td class="confirm-table__text">
                         <input type="text" readonly>
-                        <input type="hidden" name="first_name">
-                        <input type="hidden" name="last_name">
+                        <input type="text" value="{{ $contact['first_name']}} {{ $contact['first_name'] }}" readonly>
+                        <input type="hidden" name="first_name" value="{{ $contact['first_name'] }}">
+                        <input type="hidden" name="last_name" value="{{ $contact['last_name'] }}">
                     </td>
                 </tr>
                 <tr class="confirm-table__row">
@@ -28,7 +30,7 @@
                 <tr class="confirm-table__row">
                     <th class="confirm-table__header">メールアドレス</th>
                     <td class="confirm-table__text">
-                        <input type="email" name="email" readonly>
+                        <input type="email" name="email" value="{{ $contact['email'] }}" readonly>
                     </td>
                 </tr>
                 <tr class="confirm-table__row">
@@ -43,20 +45,20 @@
                 <tr class="confirm-table__row">
                     <th class="confirm-table__header">住所</th>
                     <td class="confirm-table__text">
-                        <input type="text" name="address" readonly>
+                        <input type="text" name="address" value="{{ $contact['address'] }}" readonly>
                     </td>
                 </tr>
                 <tr class="confirm-table__row">
                     <th class="confirm-table__header">建物名</th>
                     <td class="confirm-table__text">
-                        <input type="text" name="building" readonly>
+                        <input type="text" name="building" value="{{ $contact['building'] }}" readonly>
                     </td>
                 </tr>
                 <tr class="confirm-table__row">
                     <th class="confirm-table__header">お問い合わせの種類</th>
                     <td class="confirm-table__text">
-                        <input type="text" readonly>
-                        <input type="hidden" name="category_id">
+                        <input type="text" value="{{ $category->content }}" readonly>
+                        <input type="hidden" name="category_id" value="{{ $contact['category_id'] }}">
                     </td>
                 </tr>
                 <tr class="confirm-table__row">
@@ -69,7 +71,7 @@
         </div>
         <div class="form__button">
             <button class="form__button-submit" type="submit">送信</button>
-            <a href="/" class="form__button-back">修正</a>
+            <a href="{{ route('contact.index') }}" class="form__button-back">修正</a>
         </div>
     </form>
 </div>
