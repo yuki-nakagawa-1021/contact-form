f3c3ec4e9dc4a0a0ec58fcf15934fcdc84275ff6
nakagawa yuki <mstyk1021@gmail.com>
2026-02-06 02:28:50 +0900

    css設定

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -12,62 +12,57 @@
     <form class="form">
         <div class="form__group">
             <div class="form__group-title">
-                <span class="form__label--item">お名前</span>
-                <span class="form__label--required">※</span>
+                <span class="form__label--item">お名前</span><span class="form__label--required">※</span>
             </div>
             <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="text" name="first_name" placeholder="例:山田" value="{{ old('first_name') }}"/>
-                    <input type="text" name="last_name" placeholder="例:太郎" value="{{ old('last_name') }}"/>
+                <div class="form__input--text-name">
+                    <input type="text" name="first_name" placeholder="例:山田" />
+                    <input type="text" name="last_name" placeholder="例:太郎" />
                 </div>
             </div>
         </div>
         <div class="form__group">
             <div class="form__group-title">
-                <span class="form__label--item">性別</span>
-                <span class="form__label--required">※</span>
+                <span class="form__label--item">性別</span><span class="form__label--required">※</span>
             </div>
             <div class="form__group-content">
                 <div class="form__input--radio">
-                    <input type="radio" name="gender" value="1" {{ old('gender', 1) == 1 ? 'checked' : '' }}>男性
-                    <input type="radio" name="gender" value="2" {{ old('gender', 1) == 2 ? 'checked' : '' }}>女性
-                    <input type="radio" name="gender" value="3" {{ old('gender', 1) == 3 ? 'checked' : '' }}>その他
+                    <input type="radio" name="gender" value="1" >男性
+                    <input type="radio" name="gender" value="2" >女性
+                    <input type="radio" name="gender" value="3" >その他
                 </div>
             </div>
         </div><div class="form__group">
             <div class="form__group-title">
-                <span class="form__label--item">メールアドレス</span>
-                <span class="form__label--required">※</span>
+                <span class="form__label--item">メールアドレス</span><span class="form__label--required">※</span>
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}"/>
+                    <input type="email" name="email" placeholder="test@example.com" />
                 </div>
             </div>
         </div>
         <div class="form__group">
             <div class="form__group-title">
-                <span class="form__label--item">電話番号</span>
-                <span class="form__label--required">※</span>
+                <span class="form__label--item">電話番号</span><span class="form__label--required">※</span>
             </div>
             <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="tel" name="tel1" placeholder="080" value="{{ old('tel1') }}" />
+                <div class="form__input--text-tel">
+                    <input type="tel" name="tel1" placeholder="080" />
                     <span>-</span>
-                    <input type="tel" name="tel2" placeholder="1234" value="{{ old('tel2') }}"/>
+                    <input type="tel" name="tel2" placeholder="1234" />
                     <span>-</span>
-                    <input type="tel" name="tel3" placeholder="5678" value="{{ old('tel3') }}"/>
+                    <input type="tel" name="tel3" placeholder="5678" />
                 </div>
             </div>
         </div>
         <div class="form__group">
             <div class="form__group-title">
-                <span class="form__label--item">住所</span>
-                <span class="form__label--required">※</span>
+                <span class="form__label--item">住所</span><span class="form__label--required">※</span>
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="text" name="address" placeholder="例:東京都渋谷区千駄ヶ谷1-2-3" value="{{ old('address') }}"/>
+                    <input type="text" name="address" placeholder="例:東京都渋谷区千駄ヶ谷1-2-3" />
                 </div>
             </div>
         </div>
@@ -77,41 +72,34 @@
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="text" name="building" placeholder="例:千駄ヶ谷マンション101" value="{{ old('building') }}"/>
+                    <input type="text" name="building" placeholder="例:千駄ヶ谷マンション101" />
                 </div>
             </div>
         </div>
         <div class="form__group">
             <div class="form__group-title">
-                <span class="form__label--item">お問い合わせの種類</span>
-                <span class="form__label--required">※</span>
+                <span class="form__label--item">お問い合わせの種類</span><span class="form__label--required">※</span>
             </div>
             <div class="form__group-content">
                 <div class="form__input--select">
                     <select class="form__group-content-select" name="category_id">
                         <option value="">選択してください</option>
-                        @foreach ($categories as $category)
-                        <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
-                            {{ $category->content }}
-                        </option>
-                        @endforeach
                     </select>
                 </div>
             </div>
         </div>
         <div class="form__group">
             <div class="form__group-title">
-                <span class="form__label--item">お問い合わせ内容</span>
-                <span class="form__label--required">※</span>
+                <span class="form__label--item">お問い合わせ内容</span><span class="form__label--required">※</span>
             </div>
             <div class="form__group-content">
                 <div class="form__input--textarea">
-                    <textarea name="detail" placeholder="お問い合わせ内容をご記載ください">{{ old('detail') }}</textarea>
+                    <textarea name="detail" placeholder="お問い合わせ内容をご記載ください"></textarea>
                 </div>
             </div>
         </div>
         <div class="form__button">
-            <button class="form__button-submit" type="submit">送信</button>
+            <button class="form__button-submit" type="submit">確認画面</button>
         </div>
     </form>
 </div>
