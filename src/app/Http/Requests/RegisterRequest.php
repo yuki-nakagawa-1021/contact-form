fe55c3f30f7fedd26d7f2d896a49e66bdc6d2a26
nakagawa yuki <mstyk1021@gmail.com>
2026-02-08 05:04:50 +0900

    バリデーションの設定

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -24,7 +24,6 @@ class RegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            return [
             'name' => ['required'],
             'email' => ['required',  'email'],
             'password' => ['required'],
