d1a3b9ae21b0394d19eac3c9e14ded1807b77d18
nakagawa yuki <mstyk1021@gmail.com>
2026-02-08 00:21:40 +0900

    name変更

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -17,8 +17,8 @@
                     <th class="confirm-table__header">お名前</th>
                     <td class="confirm-table__text">
                         <input type="text" value="{{ $contact['last_name']}} {{ $contact['first_name'] }}" readonly>
-                        <input type="hidden" name="last_name" value="{{ $contact['last_name'] }}">
                         <input type="hidden" name="first_name" value="{{ $contact['first_name'] }}">
+                        <input type="hidden" name="last_name" value="{{ $contact['last_name'] }}">
                     </td>
                 </tr>
                 <tr class="confirm-table__row">
